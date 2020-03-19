8aa3cc2a5e4a42ed7ac9eb96f7b32b40f0cb9d79
jacksonjude <jacksonjude@mac.com>
2020-03-18 23:23:43 -0700

    Updated kick message

--- src/com/jacksonjude/OpenTime/Constants.java
+++ src/com/jacksonjude/OpenTime/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
 	public static final int TIME_COMPONENT_HOUR_MAX = 23;
 	public static final int TIME_COMPONENT_MINUTE_MAX = 59;
 	
-	public static final String KICK_REASON = "The server is closed";
+	public static final String KICK_REASON = "The server is closed (OpenTime)";
 	public static final String INT_PARSE_ERROR = "Integer parse error on <timeRangesKey>.<rangeName>.<timeKey>.<componentKey>";
 	public static final String RANGE_PARSE_ERROR = "Range parse error on <timeRangesKey>.<rangeName>.<timeKey>.<componentKey>";
 	public static final String VALUE_ERROR = "Value error on <timeRangesKey>.<rangeName>.<timeKey>.<componentKey>";
